5091af19a6566abd5b305ba44486fe47a867b460
Dybek <matdybowski@gmail.com>
2023-07-26 17:53:12 +0200

    Add websocket support

--- notes-service/src/main/scala/NotesServiceServer.scala
+++ notes-service/src/main/scala/NotesServiceServer.scala
@@ -1,8 +1,14 @@
 import cats.effect._
+import cats.syntax.applicative._
+import cats.syntax.functor._
+import cats.syntax.flatMap._
 import infrastructure.HelloWorldController
 import org.http4s.HttpRoutes
 import org.http4s.blaze.server.BlazeServerBuilder
 import org.http4s.server.Router
+import org.http4s.server.websocket.WebSocketBuilder2
+import sttp.capabilities.WebSockets
+import sttp.capabilities.fs2.Fs2Streams
 import sttp.tapir.server.ServerEndpoint
 import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
@@ -17,26 +23,34 @@ object NotesServiceServer extends IOApp {
   private val Title = "Notes Service API"
   private val Version = "0.1"
 
-  override def run(args: List[String]): IO[ExitCode] = {
+  override def run(args: List[String]): IO[ExitCode] =
+    runServer[IO]()
+
+  private def runServer[F[_]: Async](): F[ExitCode] = {
+    val async = implicitly[Async[F]]
+
     for {
-      _ <- IO(println("Starting server..."))
+      _ <- println("Starting server...").pure
 
       // routes
-      helloWorldRoutes = new HelloWorldController[IO]().all
+      helloWorldRoutes = new HelloWorldController().all
       routes = toRoutes(helloWorldRoutes)
 
       // server
-      _ <- BlazeServerBuilder[IO]
+      _ <- BlazeServerBuilder[F]
         .withExecutionContext(ec)
         .bindHttp(Port, Host)
         .withHttpApp(Router("/" -> routes).orNotFound)
         .resource
-        .use(_ => IO.never)
+        .use(_ => async.never[Unit])
     } yield ExitCode.Success
   }
 
-  private def toRoutes(logicRoutes: List[ServerEndpoint[Any, IO]]): HttpRoutes[IO] = {
+  private def toRoutes[F[_] : Async](logicRoutes: List[ServerEndpoint[Any, F]]): HttpRoutes[F] = {
     val routes = logicRoutes ++ SwaggerInterpreter().fromServerEndpoints(logicRoutes, Title, Version)
-    Http4sServerInterpreter[IO]().toRoutes(routes)
+    Http4sServerInterpreter().toRoutes(routes)
   }
+
+  private def toWebSocketRoutes[F[_] : Async](logicRoutes: List[ServerEndpoint[Fs2Streams[F] with WebSockets, F]]): WebSocketBuilder2[F] => HttpRoutes[F] =
+    Http4sServerInterpreter().toWebSocketRoutes(logicRoutes)
 }
--- notes-service/src/main/scala/infrastructure/HelloWorldController.scala
+++ notes-service/src/main/scala/infrastructure/HelloWorldController.scala
@@ -1,16 +1,44 @@
 package infrastructure
 
 import cats.effect.Async
+import cats.effect.kernel.Temporal
 import cats.syntax.applicative._
+import fs2.Stream
+import infrastructure.HelloWorldController.CountResponse
+import io.circe.generic.auto._
+import org.http4s.HttpRoutes
+import sttp.capabilities.fs2.Fs2Streams
 import sttp.tapir._
-import sttp.tapir.server.ServerEndpoint
+import sttp.tapir.generic.auto._
+import sttp.tapir.json.circe._
+import sttp.tapir.server.http4s.Http4sServerInterpreter
 
-class HelloWorldController[F[_] : Async] {
+import scala.concurrent.duration._
+
+class HelloWorldController[F[_]: Async: Temporal] {
   private val helloWorldEndpoint =
     endpoint.get
       .in("hello-world")
       .out(stringBody)
       .serverLogicSuccess(_ => "Hello world".pure[F])
 
-  val all: List[ServerEndpoint[Any, F]] = List(helloWorldEndpoint)
+  private val counterEndpoint =
+    endpoint
+      .get
+      .in("count")
+      .out(webSocketBody[String, CodecFormat.TextPlain, CountResponse, CodecFormat.Json](Fs2Streams[F]))
+      .serverLogicSuccess { _ =>
+        Stream
+          .iterate(1)(_ + 1)
+          .metered(1.second)
+          .pure
+      }
+
+  val routes: HttpRoutes[F] = Http4sServerInterpreter().toRoutes(List(helloWorldEndpoint))
+
+  val webSocketRoutes = Http4sServerInterpreter().toWebSocketRoutes(List(counterEndpoint))
+}
+
+object HelloWorldController {
+  case class CountResponse(count: Int)
 }
